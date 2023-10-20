16390cda633f36a7093eeab9f69a8d8175edea68
Yegor Tokmakov <yegor@amazon.com>
2023-10-20 16:23:13 +0200

    add missing arguments for the server;

--- app/__init__.py
+++ app/__init__.py
@@ -12,11 +12,13 @@ except ImportError:
 import bern2
 
 import time
-    
+
+
 def del_keys_from_dict(_dict, keys):
     for _key in keys:
         _dict.pop(_key, None)
     return _dict
+
 
 def create_app(args):
     app = Flask(__name__, instance_relative_config=False)
@@ -25,54 +27,62 @@ def create_app(args):
     )
 
     print(app.root_path)
-    
+
     # LOAD MODEL
     if args.front_dev:
         model = None
     else:
         model = bern2.BERN2(
-            mtner_home=args.mtner_home,
-            mtner_port=args.mtner_port,
             gnormplus_home=args.gnormplus_home,
             gnormplus_port=args.gnormplus_port,
             tmvar2_home=args.tmvar2_home,
             tmvar2_port=args.tmvar2_port,
+            mtner_home=args.mtner_home,
+            mtner_port=args.mtner_port,
             gene_norm_port=args.gene_norm_port,
             disease_norm_port=args.disease_norm_port,
-            cache_host=args.cache_host,
             cache_port=args.cache_port,
+            gnormplus_host=args.gnormplus_host,
+            tmvar2_host=args.tmvar2_host,
+            mtner_host=args.mtner_host,
+            cache_host=args.cache_host,
+            time_format=args.time_format,
+            max_word_len=args.max_word_len,
+            seed=args.seed,
             use_neural_normalizer=args.use_neural_normalizer,
-            keep_files=args.keep_files
+            keep_files=args.keep_files,
+            no_cuda=args.no_cuda
         )
-    
+
     r_parser = ResultParser()
 
     @app.route('/', methods=['GET'])
     def index():
         return render_template('index.html', debug=False)
-    
+
     @app.route('/documentation', methods=['GET'])
     def doc_view():
         return render_template('documentation.html')
-    
+
     @app.route('/debug', methods=['GET'])
     def debug():
         return render_template('index.html', debug=True)
-    
+
     @app.route('/pubmed/<pmids>', methods=['GET'])
     def pubmed_api(pmids):
         pmids = [pmid.strip() for pmid in pmids.split(",")]
         if len(pmids) == 0:
             return "[]"
-        
+
         result_dicts = [model.annotate_pmid(pmid=pmid) for pmid in pmids]
         for r in result_dicts:
             if "error_code" in r.keys():
                 if int(r["error_code"]) != 0:
                     return Response(json.dumps({"error_message": r["error_message"]}), status=404, content_type='application/json')
-            del_keys_from_dict(r, ["sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
+            del_keys_from_dict(
+                r, ["sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
         return json.dumps(result_dicts, sort_keys=True)
-    
+
     @app.route('/plain', methods=['POST'])
     def plain_api():
         params = request.get_json()
@@ -83,12 +93,11 @@ def create_app(args):
         if "error_code" in result_dict.keys():
             if int(result_dict["error_code"]) != 0:
                 return Response(json.dumps({"error_message": result_dict["error_message"]}), status=404, content_type='application/json')
-        del_keys_from_dict(result_dict, ["sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
+        del_keys_from_dict(result_dict, [
+                           "sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
 
         return json.dumps(result_dict, sort_keys=True)
 
-    
-    
     @app.route('/senddata', methods=['POST'])
     def send_data():
         start = time.time()
@@ -102,7 +111,7 @@ def create_app(args):
         if 'debug' in request.form:
             if request.form['debug'] == 'True':
                 _debug = True
-        
+
         # print("DEBUG:", _debug)
 
         if req_type == "text":
@@ -111,13 +120,16 @@ def create_app(args):
             if not args.front_dev:
                 result_dict = model.annotate_text(text=sample_text)
             else:
-                dummy_path = os.path.join(app.root_path, "temp/dummy1_20211129.json")
+                dummy_path = os.path.join(
+                    app.root_path, "temp/dummy1_20211129.json")
                 with open(dummy_path, 'r') as rf:
                     result_dict = json.load(rf)
-            _code, parse_res, keys_in_dict = r_parser.parse_result(result_dict, draw_keys, result_id="text")
+            _code, parse_res, keys_in_dict = r_parser.parse_result(
+                result_dict, draw_keys, result_id="text")
 
             if not _debug:
-                del_keys_from_dict(result_dict, ["sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
+                del_keys_from_dict(result_dict, [
+                                   "sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
 
             latency = time.time() - start
 
@@ -126,31 +138,37 @@ def create_app(args):
                 'keys': {k: r_parser.entity_type_dict[k] for k in keys_in_dict}
             })
 
-            return render_template('result_text.html', result_items=res_items, latency = f'{latency*1000:9.2f}', result_str=json.dumps(result_dict, sort_keys=True, indent=4))
+            return render_template('result_text.html', result_items=res_items, latency=f'{latency*1000:9.2f}', result_str=json.dumps(result_dict, sort_keys=True, indent=4))
         elif req_type == "pmid":
             sample_pmid = request.form['sample_text']
             _pmids = list(map(str.strip, sample_pmid.split(",")))
 
             if not args.front_dev:
-                result_dicts = [model.annotate_pmid(pmid=pmid) for pmid in _pmids]
+                result_dicts = [model.annotate_pmid(
+                    pmid=pmid) for pmid in _pmids]
             else:
-                dummy_path = os.path.join(app.root_path, "temp/dummy2_20111129.json")
+                dummy_path = os.path.join(
+                    app.root_path, "temp/dummy2_20111129.json")
                 with open(dummy_path, 'r') as rf:
                     result_dicts = json.load(rf)
-                    
-            pmid2result_dicts = {f"{result_dict['pmid']}_{i}":result_dict for i, result_dict in enumerate(result_dicts)}
+
+            pmid2result_dicts = {
+                f"{result_dict['pmid']}_{i}": result_dict for i, result_dict in enumerate(result_dicts)}
 
             for _pmid, result_dict in pmid2result_dicts.items():
-                _code, parse_res, keys_in_dict = r_parser.parse_result(result_dict, draw_keys, result_id=_pmid)
+                _code, parse_res, keys_in_dict = r_parser.parse_result(
+                    result_dict, draw_keys, result_id=_pmid)
 
                 if _code == "error":
                     # TODO: logging ERROR case
                     print("ERROR PMID:", _pmid)
-                
-                if not _debug:
-                    del_keys_from_dict(result_dict, ["sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
 
-                legend_items = {k: r_parser.entity_type_dict[k] for k in keys_in_dict}
+                if not _debug:
+                    del_keys_from_dict(result_dict, [
+                                       "sourcedb", "sourceid", "project", "elapse_time", "error_code", "error_message"])
+
+                legend_items = {
+                    k: r_parser.entity_type_dict[k] for k in keys_in_dict}
 
                 res_item = {
                     'parsed_response': parse_res,
@@ -160,10 +178,9 @@ def create_app(args):
                     res_item['title'] = result_dict['pmid']
 
                 res_items.append(res_item)
-            
-            
+
             latency = time.time() - start
 
-            return render_template('result_text.html', result_items=res_items, latency = f'{latency*1000:9.2f}', result_str=json.dumps(result_dicts, sort_keys=True, indent=4))
+            return render_template('result_text.html', result_items=res_items, latency=f'{latency*1000:9.2f}', result_str=json.dumps(result_dicts, sort_keys=True, indent=4))
 
     return app
--- server.py
+++ server.py
@@ -2,8 +2,9 @@ from app import create_app
 
 import argparse
 
+
 def parse():
-    parser = argparse.ArgumentParser(description="Arguments for BERN2")
+    parser = argparse.ArgumentParser(description="Arguments for BERN2 server")
 
     # FLASK arguments
     parser.add_argument("--host",
@@ -14,47 +15,82 @@ def parse():
                         default=5000,
                         type=int,
                         help="Port for running flask")
+    parser.add_argument("--front_dev",
+                        action="store_true")
 
     # BERN2 model arguments
-    parser.add_argument("--mtner_home",
-                        default="./multi_ner",
-                        type=str)
-    parser.add_argument("--mtner_port",
-                        default=18894,
-                        type=int)
     parser.add_argument("--gnormplus_home",
+                        help='GNormPlus home',
                         default="./resources/GNormPlusJava",
                         type=str)
     parser.add_argument("--gnormplus_port",
+                        help='GNormPlus port',
                         default=18895,
                         type=int)
     parser.add_argument("--tmvar2_home",
+                        help='tmVar 2.0 home',
                         default="./resources/tmVarJava",
                         type=str)
     parser.add_argument("--tmvar2_port",
+                        help='tmVar 2.0 port',
                         default=18896,
                         type=int)
-    parser.add_argument('--cache_host',
-                        default='localhost')
-    parser.add_argument('--cache_port',  
-                        default=27017,
+    parser.add_argument("--mtner_home",
+                        help='biomedical language model host',
+                        default="./multi_ner",
+                        type=str)
+    parser.add_argument("--mtner_port",
+                        help='biomedical language model port',
+                        default=18894,
                         type=int)
     parser.add_argument("--gene_norm_port",
+                        help='GNormPlus port',
                         default=18888,
                         type=int)
-    parser.add_argument("--disease_norm_port",
+    parser.add_argument('--disease_norm_port',
+                        help='Sieve port',
                         default=18892,
                         type=int)
-    parser.add_argument("--use_neural_normalizer",
-                        action="store_true")
-    parser.add_argument("--keep_files",
-                        action="store_true")
-    parser.add_argument("--front_dev",
-                        action="store_true")
-    parser.add_argument("--no_cuda",
-                        action="store_true")
+    parser.add_argument('--cache_port',
+                        help='annotation cached db port',
+                        default=27017,
+                        type=int)
+    parser.add_argument('--gnormplus_host',
+                        help='GNormPlus host',
+                        default='localhost',
+                        type=str)
+    parser.add_argument('--tmvar2_host',
+                        help='tmVar 2.0 host',
+                        default='localhost',
+                        type=str)
+    parser.add_argument('--mtner_host',
+                        help='biomedical language model host',
+                        default='localhost',
+                        type=str)
+    parser.add_argument('--cache_host',
+                        help='annotation cached db host',
+                        default='localhost',
+                        type=str)
+    parser.add_argument('--time_format',
+                        help='time format',
+                        default='[%d/%b/%Y %H:%M:%S.%f]',
+                        type=str)
+    parser.add_argument('--max_word_len',
+                        help='word max chars',
+                        default=50,
+                        type=int)
+    parser.add_argument("--seed",
+                        help='seed value',
+                        default=2019,
+                        type=int)
+    parser.add_argument("--use_neural_normalizer", action="store_true")
+    parser.add_argument("--keep_files", action="store_true")
+    parser.add_argument("--no_cuda", action="store_true",
+                        help="Avoid using CUDA when available")
+
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     args = parse()
